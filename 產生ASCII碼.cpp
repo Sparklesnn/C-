79dde604368c79cd42a2a933b9f90a4164e1d010
Sparklesnn <123715798+Sparklesnn@users.noreply.github.com>
2023-02-18 22:40:43 +0800

    Create 產生ASCII碼.cpp

---
+++ 產生ASCII碼.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int start,end;
+	cout<<"請輸入ASCII的起始值?";
+	cin>>start;
+	cout<<"請輸入ASCII的終止值?";
+	cin>>end;
+	for(int i=start;i<=end;i++){
+		cout<<(char)i<<endl;
+	} 
+	return 0;
+}
